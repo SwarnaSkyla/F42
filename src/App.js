4ff704f5f7c1aa9c7afaf07e1131820a5ad62138
SwarnaSkyla <117882181+SwarnaSkyla@users.noreply.github.com>
2023-06-24 21:07:35 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './Component/Login';
+import Profile from './Component/Profile';
+
+const App=()=>{
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login/>}/>
+        <Route path="/profile" element={<Profile/>}/>
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
+
+
+
+
